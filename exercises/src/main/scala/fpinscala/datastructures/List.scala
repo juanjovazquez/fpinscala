c2be44884ae5c8667740482bea10eac370ac2e45
Juan José Vázquez <juanjo.vazquez.delgado@tecsisa.com>
2017-02-16 18:56:05 +0100

    Ch03 Exercises (Functional Data Structures)

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -31,6 +31,8 @@ object List { // `List` companion object. Contains functions for creating and wo
     case _ => 101
   }
 
+  // Ex. 3.1: the value of `x` will be `3`. 
+
   def append[A](a1: List[A], a2: List[A]): List[A] =
     a1 match {
       case Nil => a2
@@ -50,19 +52,140 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(ns, 1.0)(_ * _) // `_ * _` is more concise notation for `(x,y) => x * y`; see sidebar
 
 
-  def tail[A](l: List[A]): List[A] = ???
+  def tail[A](l: List[A]): List[A] = l match {
+    case Nil => sys.error("empty list")
+    case Cons(_, xs) => xs
+  }
 
-  def setHead[A](l: List[A], h: A): List[A] = ???
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => sys.error("empty list")
+    case Cons(_, xs) => Cons(h, xs)
+  }
 
-  def drop[A](l: List[A], n: Int): List[A] = ???
+  def drop[A](l: List[A], n: Int): List[A] = {
+    @annotation.tailrec
+    def go(rest: List[A], i: Int): List[A] = rest match {
+      case Nil => sys.error("empty list")
+      case Cons(x, xs) =>
+        if (i < n) go(xs, i + 1) else rest
+    }
+    go(l, 0)
+  }
 
-  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = ???
+  def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
+    case Nil => Nil
+    case Cons(x, xs) => if (f(x)) dropWhile(xs, f) else l
+  }
 
-  def init[A](l: List[A]): List[A] = ???
+  def init[A](l: List[A]): List[A] = {
+    val buffer = new scala.collection.mutable.ListBuffer[A]
+    @annotation.tailrec
+    def go(rest: List[A]): List[A] = rest match {
+      case Nil => sys.error("empty list")
+      case Cons(_, Nil) =>
+        List(buffer.toList: _*)
+      case Cons(x, xs) =>
+        buffer.append(x)
+        go(xs)
+    }
+    go(l)
+  }
 
-  def length[A](l: List[A]): Int = ???
+  def length[A](l: List[A]): Int =
+    foldRight(l, 0)((_, accum) => accum + 1)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+  }
 
-  def map[A,B](l: List[A])(f: A => B): List[B] = ???
+  def sumViaFoldLeft(l: List[Int]): Int =
+    foldLeft(l, 0)(_ + _)
+
+  def productViaFoldLeft(l: List[Double]): Double =
+    foldLeft(l, 1.0)(_ * _)
+
+  def lengthViaFoldLeft[A](l: List[A]): Int =
+    foldLeft(l, 0)((accum, _) => accum + 1)
+
+  def reverse[A](l: List[A]): List[A] =
+    foldLeft(l, Nil: List[A])((accum, a) => Cons(a, accum))
+
+  def foldLeftViaFoldRight[A, B](l: List[A], z: B)(f: (B, A) => B): B =
+    foldRight(l, (b: B) => b)((a, g) => b => g(f(b, a)))(z)
+
+  def foldRightViaFoldLeft[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(l, (b: B) => b)((g, a) => b => g(f(a, b)))(z)
+
+  def appendViaFoldRight[A](a1: List[A], a2: List[A]): List[A] =
+    foldRight(a1, a2)(Cons(_, _))
+
+  def concat[A](ll: List[List[A]]): List[A] =
+    foldLeft(ll, List[A]())(append)
+
+  def map[A,B](l: List[A])(f: A => B): List[B] = l match{
+    case Nil => Nil
+    case Cons(x, xs) => Cons(f(x), map(xs)(f))
+  }
+
+  def mapViaFoldRight[A, B](l: List[A])(f: A => B): List[B] =
+    foldRight(l, List[B]())((a, accum) => Cons(f(a), accum))
+
+  def plusOne(l: List[Int]): List[Int] =
+    map(l)(_ + 1)
+
+  def doublesToStrings(l: List[Double]): List[String] =
+    map(l)(_.toString)
+
+  def filter[A](l: List[A])(f: A => Boolean): List[A] =
+    foldRightViaFoldLeft(l, List[A]())((a, accum) => if (f(a)) Cons(a, accum) else accum)
+
+  def removeOdds(l: List[Int]): List[Int] =
+    filter(l)(_ % 2 == 0)
+
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] =
+    concat(map(l)(f))
+
+  def filterViaFlatMap[A](l: List[A])(f: A => Boolean): List[A] =
+    flatMap(l)(a => if (f(a)) Cons(a, Nil) else Nil)
+
+  def addPairwise(a1: List[Int], a2: List[Int]): List[Int] = (a1, a2) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(x1, xs1), Cons(x2, xs2)) => Cons(x1 + x2, addPairwise(xs1, xs2))
+  }
+
+  def zipWith[A](a1: List[A], a2: List[A])(f: (A, A) => A): List[A] = (a1, a2) match {
+    case (Nil, _) => Nil
+    case (_, Nil) => Nil
+    case (Cons(x1, xs1), Cons(x2, xs2)) => Cons(f(x1, x2), zipWith(xs1, xs2)(f))
+  }
+
+  def exists[A](l: List[A])(f: A => Boolean): Boolean = l match {
+    case Nil => false
+    case Cons(x, xs) => if (f(x)) true else exists(xs)(f)
+  }
+
+  // Not very efficient as it computes strictly
+  // all possible combinations in advance (lazyness would help).
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = {
+    def sequences[A](l: List[A]): List[List[A]] = {
+      val buffer = new scala.collection.mutable.ListBuffer[List[A]]
+      @annotation.tailrec
+      def go(accum: List[A], rest: List[A]): List[List[A]] = {
+        buffer += reverse(accum)
+        rest match {
+          case Nil => List(buffer.toList: _*)
+          case Cons(x, xs) => go(Cons(x, accum), xs)
+        }
+      }
+      l match {
+        case Nil => Nil
+        case Cons(x, xs) => append(go(List(x), xs), sequences(xs))
+      }
+    }
+    exists(sequences(sup))(_ == sub)
+  }
+
 }
--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -7,7 +7,41 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
 
+  def size[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 1
+    case Branch(l, r) => 1 + size(l) + size(r)
+  }
 
+  def max(t: Tree[Int]): Int = t match {
+    case Leaf(x) => x
+    case Branch(l, r) => max(l) max max(r)
+  }
 
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
+
+  def map[A, B](t: Tree[A])(f: A => B): Tree[B] = t match {
+    case Leaf(x) => Leaf(f(x))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](t: Tree[A])(f: A => B)(g: (B, B) => B): B = t match {
+    case Leaf(a) => f(a)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def sizeViaFold[A](t: Tree[A]): Int =
+    fold(t)(_ => 1)(1 + _ + _)
+
+  def maxViaFold(t: Tree[Int]): Int =
+    fold(t)(identity)(_ max _)
+
+  def depthViaFold[A](t: Tree[A]): Int =
+    fold(t)(_ => 0)(1 + _ max _)
+
+  def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] =
+    fold(t)(a => Leaf(f(a)): Tree[B])(Branch(_, _))
 
 }
